401b73fed2db64995d42c14c32867a471866e184
blue-beaker <yyy5566778899@126.com>
2024-12-08 11:44:33 +0800

    init

---
+++ src/main/java/io/bluebeaker/jemicroblocks/JEMicroblocksConfig.java
@@ -0,0 +1,13 @@
+package io.bluebeaker.jemicroblocks;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.LangKey;
+import net.minecraftforge.common.config.Config.Type;
+
+@Config(modid = JEMicroblocks.MODID,type = Type.INSTANCE,category = "general")
+public class JEMicroblocksConfig {
+//    @Comment("Example")
+//    @LangKey("config.jemicroblocks.example.name")
+//    public static boolean example = true;
+}
