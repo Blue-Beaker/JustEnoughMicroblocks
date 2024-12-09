bf215a04a8982254a1a923ce3590c28b4280c3fb
blue-beaker <yyy5566778899@126.com>
2024-12-09 08:35:28 +0800

    config and info

--- src/main/java/io/bluebeaker/jemicroblocks/JEMicroblocksConfig.java
+++ src/main/java/io/bluebeaker/jemicroblocks/JEMicroblocksConfig.java
@@ -7,10 +7,13 @@ import net.minecraftforge.common.config.Config.Type;
 
 @Config(modid = JEMicroblocks.MODID,type = Type.INSTANCE,category = "general")
 public class JEMicroblocksConfig {
+    
+    @Config.RequiresMcRestart
     @Comment("Show recipes for only selected blocks to reduce startup time and memory usage. ")
     @LangKey("config.jemicroblocks.show_less.name")
     public static boolean show_less = false;
 
+    @Config.RequiresMcRestart
     @Comment("Blocks to show recipes for, when show_less is enabled. Format:'modid:id', metadata is ignored")
     @LangKey("config.jemicroblocks.shown_blocks.name")
     public static String[] shown_blocks = {
